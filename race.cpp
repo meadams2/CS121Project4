eb111b02b14b08597d068fba26f8d80c86c4239e
Marianne Adams <meadams2@bsu.edu>
2025-02-07 14:15:00 +0000

    Initializing constants in Race::Race()

--- race.cpp
+++ race.cpp
@@ -5,20 +5,29 @@
 #include "horse.h"
 #include "race.h"
 
-Race::Race(){
-	Race::trackLength = 15;
-	Race::numHorses = 5;
-	Race::horses[i];
+Race::Race() : trackLength(15), numHorses(5){
+//	trackLength = 15;
+//	numHorses = 5;
+	horses[numHorses];
 	for (int i = 0; i < numHorses; i++){
-		Horse::horses[i];
-		horses[i] = Horse::init(i + 1, trackLength);
+		horse[i] = Horse()
+		Horse::init(i, trackLength);
 	}//end for loop
 }//end constructor
 
 void Race::run(){
-	for (int i = 0; i < numHorses; i++){
-		Horse::run();
-	}//end for loop
+	bool keepGoing = true;
+	while (keepGoing){
+		for (int i = 0; i < numHorses; i++){
+			Horse::advance();
+			Horse::printLane();
+			if (Horse::isWinner()){
+				std::cout << "Horse" << i << "is the winner!";
+				keepGoing = false;
+			}//end condition
+		}//end for loop
+		std::cin.ignore();
+	}//end while loop
 }//end run
 
 
